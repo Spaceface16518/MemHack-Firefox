a1752347667001bf10f07cc21b99159663aa1364
Amrit <33424649+Spaceface16518@users.noreply.github.com>
2018-03-02 08:58:23 -0600

    Try click event instead of submit

--- content-scripts/autoPass.js
+++ content-scripts/autoPass.js
@@ -1,4 +1,4 @@
 var elem = document.getElementById("pass__pass");
-var event = new Event("submit"); // (*)
+var event = new Event("click"); // (*)
 elem.dispatchEvent(event);
 console.log('[MemHack] question passed')
