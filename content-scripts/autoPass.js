db5e4904a1d01fa34da68761e6840b8f583540b5
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-02 11:30:02 -0600

    Another try

--- content-scripts/autoPass.js
+++ content-scripts/autoPass.js
@@ -1,4 +1,6 @@
 var elem = document.getElementById("pass");
+elem.style.display = "inline"
 var event = new Event("submit");
 elem.dispatchEvent(event);
 console.log('[MemHack] question passed');
+elem.style.display = "hidden"
